8ecc8a936fb744d7fbcd91f6e9be24993296a08b
i.kalinichenko@ispsystem.com <i.kalinichenko@ispsystem.com>
2022-04-29 15:48:58 +0000

    add ssh pubkeys

--- client.go
+++ client.go
@@ -536,7 +536,7 @@ func (c *Client) ChangeAccountRole(id string, role string) (err error) {
 }
 // Add ssh public key to account
 func (c *Client) AccountAddSshKey(id string, key SshKeyConfig) (err error) {
-	url := fmt.Sprintf("auth/v3/user/%v/sshkey", id)
+	url := fmt.Sprintf("/auth/v3/user/%v/sshkey", id)
 	config := map[string]string{
 		"name": key.Name,
 		"ssh_pub_key": key.Key,
@@ -547,7 +547,7 @@ func (c *Client) AccountAddSshKey(id string, key SshKeyConfig) (err error) {
 // Get set of ssh public keys from account 
 func (c *Client) AccountGetSshKeys(id string) (ssh_keys []interface{}, err error) {
 	var data map[string]interface{}
-	url := fmt.Sprintf("auth/v3/user/%v/sshkey", id)
+	url := fmt.Sprintf("/auth/v3/user/%v/sshkey", id)
         err = c.GetJsonRetryable(url, &data, 3)
 	if err != nil {
 		return nil, err
@@ -555,6 +555,6 @@ func (c *Client) AccountGetSshKeys(id string) (ssh_keys []interface{}, err error
 	if len(data["list"].([]interface{})) == 0 {
 		return nil, nil
 	}
-	ssh_keys := data["list"].([]interface{})
+	ssh_keys = data["list"].([]interface{})
 	return
 }
